23d51f4bc608f82b16acac7cd9467d086d03d9aa
Luis Comezaquira <lecomezaquira83@ucatolica.edu.co>
2025-03-02 17:14:45 -0500

    feat: se crearon las interfaces que se implementaran

---
+++ src/main/java/avesV3/TipoSonido.java
@@ -0,0 +1,8 @@
+package avesV3;
+
+public interface TipoSonido {
+    /**
+     * Esta interfaz permite a una clase definir el tipo de sonido que puede hacer.
+     */
+    void makeSound();
+}
---
+++ src/main/java/avesV3/TipoVuelo.java
@@ -0,0 +1,8 @@
+package avesV3;
+
+public interface TipoVuelo {
+    /**
+     * Esta interfaz permite a una clase definir el tipo de vuelo que puede hacer.
+     */
+    void vuelo();
+}
